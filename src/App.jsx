cfc55ba225d103480c80c43aba1783d2998ea4a2
ubuntu <aitechmanml@gmail.com>
2025-04-14 15:17:44 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,36 @@ function App() {
   // State and logic for navigation/content will go here later
 
   return (
-    <div className="relative z-10 min-h-screen flex flex-col items-center justify-center text-white">
+    <div className="relative z-10 min-h-screen flex flex-col items-center text-white p-8"> {/* Added padding */}
       {/* Placeholder Content */}
       <h1 className="text-4xl font-bold mb-4">My Portfolio</h1>
-      <p>Welcome to my corner of the universe.</p>
+      <p className="mb-4">Welcome to my corner of the universe.</p>
 
       {/* Canvas for 3D elements will be added here */}
 
       {/* Other sections (Planets) will be managed here */}
+
+      {/* Add Lorem Ipsum for scroll testing */}
+      <div className="max-w-3xl text-justify space-y-4 mt-10">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Nunc sed id semper risus in hendrerit gravida rutrum. Amet porttitor eget dolor morbi non arcu risus quis. Viverra justo nec ultrices dui sapien eget mi proin. Nibh praesent tristique magna sit amet purus gravida quis. Eget nullam non nisi est sit amet facilisis magna. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Orci eu lobortis elementum nibh tellus molestie nunc non. Massa tincidunt dui ut ornare lectus sit amet est placerat.
+        </p>
+        <p>
+          Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Purus sit amet volutpat consequat mauris nunc congue nisi. Sed vulputate mi sit amet mauris commodo quis imperdiet. Tellus elementum sagittis vitae et leo duis ut diam. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Nisl tincidunt eget nullam non nisi est sit amet. Consequat id porta nibh venenatis cras sed felis eget velit. Faucibus pulvinar elementum integer enim neque volutpat ac.
+        </p>
+        <p>
+          Elementum curabitur vitae nunc sed velit dignissim sodales ut. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam. Ultrices sagittis orci a scelerisque purus semper eget duis. Tortor consequat id porta nibh venenatis cras sed felis. Id aliquet risus feugiat in ante metus dictum at tempor. Mattis rhoncus urna neque viverra justo nec ultrices dui. Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus.
+        </p>
+        <p>
+          Volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Amet consectetur adipiscing elit ut aliquam purus sit amet. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Nunc consequat interdum varius sit amet mattis vulputate enim. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Vitae congue eu consequat ac felis donec et odio. Eget mauris pharetra et ultrices neque ornare aenean euismod.
+        </p>
+        <p>
+          Lacus laoreet non curabitur gravida arcu ac tortor dignissim. Sed nisi lacus sed viverra tellus in hac habitasse. Morbi tincidunt augue interdum velit euismod in pellentesque massa. Tellus orci ac auctor augue mauris augue neque gravida. Erat nam at lectus urna duis convallis convallis tellus. Nunc mattis enim ut tellus elementum sagittis vitae et leo. Ut sem viverra aliquet eget sit amet tellus cras adipiscing.
+        </p>
+      </div>
     </div>
   )
 }
